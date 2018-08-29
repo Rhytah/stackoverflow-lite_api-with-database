2066eae93303160b6771300eafc911b81b2a03b1
Rhytah <ritanamono@gmail.com>
2018-08-30 02:36:04 +0300

    define signup route

--- api/views.py
+++ api/views.py
@@ -1,45 +1,88 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask,request,jsonify,make_response
+#from flask_restful import Resource, Api
+from .models.api_models import User
+from api.models.database import DbManager
+
 
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
+dbmanager=DbManager()
 
-class Register(Resource):
-    def post(self):
-        return {'message': 'Register user'}
+# class Register(Resource):def post(self):
+@app.route('/api/v2/auth/signup', methods =['POST'])
+def user_signup():
+    
+        user_data = request.get_json()
+        name = user_data['name']
+        username = user_data['username']
+        password = user_data['password']
 
-api.add_resource(Register, '/auth/signup')
+        if not user_data:
+            return jsonify({'status':'Fail',
+                  'message':'All fields are required'})
 
-class Login(Resource):
-    def post(self):
-        return {'messgae':'user login'}
+        if not name or name == "  ":
+            return jsonify({"message": 'Invalid, Please enter a correct name'})
 
-api.add_resource(Login, '/auth/login')
+        if not username or username == "  ":
+            return jsonify({"message": 'Invalid, Please enter a valid username'}) 
 
-class Allquestions(Resource):
-    def get(self):
-        return {'message':'Fetch all questions'}
+        if not password or password == "  ":
+            return jsonify({"message": 'Invalid, Please enter a correct password'})
 
-    def post(self):
-        return {'message': 'Add questions'}
+        if dbmanager.get_by_argument(name,username,password):
+            return jsonify({'message':'User already exists'})
+        dbmanager.register_user(name,username,password)
+        return jsonify({'message':'user added successfully'}),201
 
-api.add_resource(Allquestions, '/api/v2/questions')
+@app.route('/api/v2/auth/login', methods =['POST'])
+def login_user():
+    user_data=request.get_json()
+    name=data['name']
+    username=data['username']
+    password=data['password']
+    dbmanager_query=dbmanager.get_by_argument('users','username',username)
+    user=User(dbmanager_query[0], dbmanager_query[1], dbmanager_query[2], dbmanager_query[3])
 
-class Specific_question(Resource):
-    def get(self, question_id):
-        return {'message':'fetch a specific question'}
 
-    def delete(self,question_id):
-        return {'message':'delete a question'}
+    
+# api.add_resource(Register, '/api/v2/auth/signup')
 
-api.add_resource(Specific_question, '/api/v2/questions/question_id')
+# class Login(Resource):
+#     def post(self):
+#         return {'messgae':'user login'}
 
-class Answertoqtn(Resource):
-    def post(self, question_id):
-        return {'message':'post an answer to a question'}
+# api.add_resource(Login, '/auth/login')
 
-api.add_resource(Answertoqtn, '/api/v2/questions/question_id/answers')
+# class Allquestions(Resource):
+#     def get(self):
+#         request_data=request.get_json()
+
+#         return {'message':'Fetch all questions'}
+
+#     def post(self):
+#         return {'message': 'Add questions'}
+
+# api.add_resource(Allquestions, '/api/v2/questions')
+
+# class Specific_question(Resource):
+#     def get(self, question_id):
+#         return {'message':'fetch a specific question'}
+
+#     def delete(self,question_id):
+#         return {'message':'delete a question'}
+
+# api.add_resource(Specific_question, '/api/v2/questions/question_id')
+
+# class Answertoqtn(Resource):
+#     def post(self, question_id):
+#         return {'message':'post an answer to a question'}
+
+# api.add_resource(Answertoqtn, '/api/v2/questions/question_id/answers')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # db=DbManager()
+    # db.create_tables()
+    
