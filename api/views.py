750785fb2f2867d19d459987f7878564df54c63d
Rhytah <ritanamono@gmail.com>
2018-08-30 16:52:56 +0300

    protection to routes

--- api/views.py
+++ api/views.py
@@ -1,17 +1,18 @@
 from flask import Flask,request,jsonify,make_response
-#from flask_restful import Resource, Api
 from .models.api_models import User, Question
 from api.models.database import DbManager
-
 from datetime import datetime,timedelta
+from flask_jwt_extended import(JWTManager,jwt_required,create_access_token,get_jwt_identity)
 
 
 
 app = Flask(__name__)
-#api = Api(app)
+ #setup flask-jwt-extened entension
+app.config['JWT_SECRET_KEY'] = 'rhytahz'
+jwt = JWTManager(app)
 dbmanager=DbManager()
 
-# class Register(Resource):def post(self):
+
 @app.route('/api/v2/auth/signup', methods =['POST'])
 def user_signup():
     
@@ -44,28 +45,48 @@ def login_user():
     username=user_data['username']
     password=user_data['password']
     dbmanager_query=dbmanager.get_by_argument('users','username',username)
-    user=User(dbmanager_query[0], dbmanager_query[1], dbmanager_query[2], dbmanager_query[3])
+    # user=User(dbmanager_query[0], dbmanager_query[1], dbmanager_query[2], dbmanager_query[3])
     
-    print (user)
+    # print (user)
+    if not username:
+        return jsonify({"msg" : "Provide Valid Username"}),400
+
+    if not password:
+        return jsonify({"msg" : "Incorrect password"}),400
+
+    # if username !='test' or password !='test':
+    #     return jsonify({"msg": "Invalid username or password"})
+
+    access_token= create_access_token(identity=username)
+    return jsonify(access_token=access_token),200
+
+@app.route('/api/v2/protected', methods =['GET'])
+@jwt_required
+def protected():
+    #access the identity of current user
+    current_user=get_jwt_identity()
+    return jsonify(loggesd_in_as=current_user),200
     
-    if user.username ==user_data['username']:
-        token = jwt.encode(
-                        {'username': user.username,
-                         'exp': datetime.utcnow() +
-                         timedelta(days=10, minutes=60)
-                         }, 'mysecret')
-        if token:
-            response = {
-                'message': 'You logged in successfully.',
-                'token': token.decode('UTF-8'),
-                'name': user.name,
-                'username': user.username,
-                'user_id': user.user_id
-            }
-        return make_response(jsonify(response)), 200
-    return jsonify({'message':'Invalid username or password, try again or create an account'}), 401
-    
+    # if user.username ==user_data['username']:
+    #     token = jwt.encode(
+    #                     {'username': user.username,
+    #                      'exp': datetime.utcnow() +
+    #                      timedelta(days=10, minutes=60)
+    #                      }, 'mysecret')
+    #     if token:
+    #         response = {
+    #             'message': 'You logged in successfully.',
+    #             'token': token.decode('UTF-8'),
+    #             'name': user.name,
+    #             'username': user.username,
+    #             'user_id': user.user_id
+    #         }
+    #     return make_response(jsonify(response)), 200
+        
+    # return jsonify({'message':'Invalid username or password, try again or create an account'}), 401
+
 @app.route('/api/v2/questions', methods =['GET'])
+@jwt_required
 def fetch_questions():
     questions= dbmanager.get_questions()   
     if len(questions)<1:
@@ -82,6 +103,7 @@ def fetch_questions():
  
     return jsonify({'questions':questions})
 @app.route('/api/v2/questions', methods=['POST'])
+@jwt_required
 def add_question():
     #questions=dbmanager.get_questions()
     request_data=request.get_json()
@@ -94,6 +116,7 @@ def add_question():
 
 
 @app.route('/api/v2/question/<int:question_id>', methods=['GET'])
+@jwt_required
 def fetch_a_question(question_id):
     
     question=dbmanager.get_a_question()
