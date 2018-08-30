8eae16043605fccdd348f80673fbe1f29794e3bb
Rhytah <ritanamono@gmail.com>
2018-08-30 12:14:26 +0300

    fetch all questions

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from flask import Flask,request,jsonify,make_response
 #from flask_restful import Resource, Api
-from .models.api_models import User
+from .models.api_models import User, Question
 from api.models.database import DbManager
+import jwt
+from datetime import datetime,timedelta
+
 
 
 app = Flask(__name__)
@@ -12,37 +15,74 @@ dbmanager=DbManager()
 @app.route('/api/v2/auth/signup', methods =['POST'])
 def user_signup():
     
-        user_data = request.get_json()
-        name = user_data['name']
-        username = user_data['username']
-        password = user_data['password']
+    user_data = request.get_json()
+    name = user_data['name']
+    username = user_data['username']
+    password = user_data['password']
 
-        if not user_data:
-            return jsonify({'status':'Fail',
-                  'message':'All fields are required'})
+    if not user_data:
+        return jsonify({'status':'Fail',
+                'message':'All fields are required'})
 
-        if not name or name == "  ":
-            return jsonify({"message": 'Invalid, Please enter a correct name'})
+    if not name or name == "  ":
+        return jsonify({"message": 'Invalid, Please enter a correct name'})
 
-        if not username or username == "  ":
-            return jsonify({"message": 'Invalid, Please enter a valid username'}) 
+    if not username or username == "  ":
+        return jsonify({"message": 'Invalid, Please enter a valid username'}) 
 
-        if not password or password == "  ":
-            return jsonify({"message": 'Invalid, Please enter a correct password'})
+    if not password or password == "  ":
+        return jsonify({"message": 'Invalid, Please enter a correct password'})
 
-        if dbmanager.get_by_argument(name,username,password):
-            return jsonify({'message':'User already exists'})
-        dbmanager.register_user(name,username,password)
-        return jsonify({'message':'user added successfully'}),201
+    
+    dbmanager.register_user(name,username,password)
+    return jsonify({'message':'user added successfully'}),201
 
 @app.route('/api/v2/auth/login', methods =['POST'])
 def login_user():
     user_data=request.get_json()
-    name=data['name']
-    username=data['username']
-    password=data['password']
+    #name=user_data['name']
+    username=user_data['username']
+    password=user_data['password']
     dbmanager_query=dbmanager.get_by_argument('users','username',username)
     user=User(dbmanager_query[0], dbmanager_query[1], dbmanager_query[2], dbmanager_query[3])
+    
+    print (user)
+    
+    if user.username ==user_data['username']:
+        token = jwt.encode(
+                        {'username': user.username,
+                         'exp': datetime.utcnow() +
+                         timedelta(days=10, minutes=60)
+                         }, 'mysecret')
+        if token:
+            response = {
+                'message': 'You logged in successfully.',
+                'token': token.decode('UTF-8'),
+                'name': user.name,
+                'username': user.username,
+                'user_id': user.user_id
+            }
+        return make_response(jsonify(response)), 200
+    return jsonify({'message':'Invalid username or password, try again or create an account'}), 401
+    
+@app.route('/api/v2/questions', methods =['GET'])
+def fetch_questions():
+    questions= dbmanager.get_questions()   
+    if len(questions)<1:
+        return jsonify({
+            "status":'Fail',
+            "message":'There are no questions'
+        }),404
+
+    if len(questions)>=1:
+        return jsonify({
+            "message":'Here are the questions',
+            "questions":questions
+        })
+ 
+    return jsonify({'questions':questions})
+
+
 
 
     
